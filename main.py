f67ccaf3d851664df972481ef3459837f750458b
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-07-02 21:26:51 +0000

    few comments

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}
 
+#refactor to include the extraction options as well
 @app.post("/extract/")
 async def extract_text(file: UploadFile = File(description="Extract files.")):
     file_bytes = await file.read()
@@ -26,6 +27,7 @@ async def summarize_route(text: TextInput):
     summary = summarize_text(text.text)
     return {"summary": summary}
 
+#test this endpoint to see if it works as expected
 @app.post("/upload-and-summarize/", response_model=SummaryText)
 async def upload_and_summarize(file: UploadFile = File(description="Upload file, extract text and summarize.")):
     file_bytes = await file.read()
