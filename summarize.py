49f7434f10e594fb6bc6e7c18171cde09b9ee0ae
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-07-05 15:31:23 +0000

    reversed bck to the previous model

--- summarize.py
+++ summarize.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_text(text: str):
     truncated_text = text[:1000]
